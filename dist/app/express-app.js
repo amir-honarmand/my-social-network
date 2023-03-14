4eb612cf7fe90abe5ce5140dd58dda53e51e7902
amir honarmand <amir.honarmand.zade@gmail.com>
2023-03-15 02:54:01 +0330

    added morgan

--- dist/app/express-app.js
+++ dist/app/express-app.js
@@ -6,9 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const router_1 = __importDefault(require("./router"));
+const morgan_1 = __importDefault(require("morgan"));
 exports.default = async (app) => {
     app.use(express_1.default.json());
     app.use((0, cors_1.default)());
+    app.use((0, morgan_1.default)('dev'));
     app.use('/v1', router_1.default);
     app.all('*', (req, res) => {
         res.status(404).json({
--- src/app/express-app.ts
+++ src/app/express-app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import router from './router';
+import morgan from 'morgan';
 
 export default async (app: express.Express) => {
     app.use(express.json());
     app.use(cors());
+    app.use(morgan('dev'));
     app.use('/v1', router);
 
     app.all('*', (req, res) => {
